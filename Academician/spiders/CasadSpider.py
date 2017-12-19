46310247966194304942c6eaf644afff34d30bab
Tilyp <13661944195@139.com>
2017-12-20 00:38:10 +0800

    院士

--- Academician/spiders/CasadSpider.py
+++ Academician/spiders/CasadSpider.py
@@ -2,6 +2,7 @@
 from scrapy import Request
 from scrapy.spiders import CrawlSpider
 from bs4 import BeautifulSoup
+import requests
 
 class CasadSpider(CrawlSpider):
 
@@ -19,5 +20,13 @@ class CasadSpider(CrawlSpider):
 
     def parse_detail(self, response):
         soup = BeautifulSoup(response.body, "lxml")
-        print soup
+        div = soup.find("div", class_="contentTest")
+        contentTest = div.get_text(strip=True)
+        acadImg = div.find("div", class_="acadImg").find("img")
+        img = acadImg["src"]
+        file_data = requests.get(img).content
+        name = acadImg["alt"]
+        with open("data/" + name + ".png", "wb") as fd:
+            fd.write(file_data)
+        print contentTest
 
